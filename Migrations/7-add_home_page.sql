bd4b99cbb4d658b7bff16bbe3ca720c2dd2a8ad7
loppys <xteri.yt@gmail.com>
2022-05-27 23:42:07 +0300

    Фикс миграции

--- Migrations/7-add_home_page.sql
+++ Migrations/7-add_home_page.sql
@@ -1,2 +1,2 @@
 INSERT INTO `template` (`page`, `group`, `tpl`, `js`, `path`) VALUES ('home', 'home', NULL, NULL, 'WWW');
-INSERT INTO `engine`.`pages` (`name`, `url`, `default`, `template`) VALUES ('Главная', 'home', '1', 'home');
+INSERT INTO `pages` (`name`, `url`, `default`, `template`) VALUES ('Главная', 'home', '1', 'home');

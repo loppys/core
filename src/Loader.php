59e1d3c1866af91090fc3bfd4fd27bf6ffc9eb79
loppys <xteri.yt@gmail.com>
2022-10-04 22:42:46 +0300

    Получение объекта кэша по имени

--- src/Loader.php
+++ src/Loader.php
@@ -1,6 +1,31 @@
 <?php
 
+use Vengine\System\Cache;
+use Vengine\Packages\Settings\Storages\ConstStorage;
+use Loader\Builder\Storage;
+
 class Loader extends \Loader\Process
 {
-  
+  public static function getCacheObject(string $name): ?Cache
+  {
+    if (ConstStorage::CACHE_ENABLED !== true) {
+      return null;
+    }
+
+    $fullName = md5('cache' . $name);
+
+    if (Storage::has($fullName)) {
+      return parent::callModule($fullName);
+    }
+
+    $data = [
+      'name' => $fullName,
+      'handler' => \Vengine\System\Cache::class,
+      'param' => [$name],
+    ];
+
+    Storage::add($fullName, Storage::GROUP_COMPONENT, $data);
+
+    return self::getCacheObject($name);
+  }
 }

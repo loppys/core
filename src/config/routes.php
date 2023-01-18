4131eb330baf896a132165abd14a7b564e1683ad
loppys <xteri.yt@gmail.com>
2023-01-18 21:20:21 +0300

    Роуты

--- src/config/routes.php
+++ src/config/routes.php
@@ -1,3 +1,32 @@
 <?php
 
-return [];
+use Vengine\System\Settings\Storages\MethodType;
+use Vengine\Packages\Updater\Components\RenderUpdater;
+use Vengine\System\Components\Page\Home\HomePageController;
+use Vengine\System\Settings\Storages\AccessLevelStorage;
+
+return [
+    [
+        'route' => '/home/',
+        'method' => MethodType::GET,
+        'handler' => HomePageController::class,
+    ],
+    [
+        'route' => '/install/',
+        'method' => MethodType::GET,
+        'handler' => [
+            'controller' => RenderUpdater::class,
+            'method' => 'process',
+            'access' => AccessLevelStorage::ALL
+        ]
+    ],
+    [
+        'route' => '/install/step/{num:\d+}',
+        'method' => MethodType::GET,
+        'handler' => [
+            'controller' => RenderUpdater::class,
+            'method' => 'changeStep',
+            'access' => AccessLevelStorage::ROOT
+        ]
+    ],
+];

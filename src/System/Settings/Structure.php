e63a14de25f2cc0c71b36787f351c6a912c189da
loppys <xteri.yt@gmail.com>
2023-01-19 22:11:23 +0300

    Правка структуры

--- src/System/Settings/Structure.php
+++ src/System/Settings/Structure.php
@@ -6,10 +6,10 @@ use Vengine\AbstractConfig;
 
 /**
  * @property string vendor
- * @property string loader
- * @property string core
+ * @property string container
  * @property string coreConfig
  * @property string userConfig
+ * @property string core
  * @property string api
  * @property string uApi
  */

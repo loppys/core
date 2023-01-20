e0aed71b4e07a3d37028d90f89ca397472e4e7d6
loppys <xteri.yt@gmail.com>
2023-01-20 08:52:15 +0300

    Выноска функции + поправил путь

--- install.php
+++ install.php
@@ -2,39 +2,41 @@
 
 $ds = DIRECTORY_SEPARATOR;
 
+if (!function_exists('fullCopy')) {
+    function fullCopy($source, $target)
+    {
+        if (is_dir($source)) {
+
+            @mkdir($target);
+
+            $dir = dir($source);
+
+            while (($entry = $dir->read()) !== false) {
+                if ($entry === '.' || $entry === '..') {
+                    continue;
+                }
+
+                fullCopy(
+                    $source . DIRECTORY_SEPARATOR . $entry,
+                    $target . DIRECTORY_SEPARATOR . $entry
+                );
+            }
+
+            $dir->close();
+        } else {
+            copy($source, $target);
+        }
+    }
+}
+
 if (
     !is_dir($_SERVER['DOCUMENT_ROOT'] . $ds . 'www')
     && !file_exists($_SERVER['DOCUMENT_ROOT'] . $ds . 'config' . $ds . 've.config')
 ) {
-    if (!function_exists('fullCopy')) {
-        function fullCopy($source, $target)
-        {
-            if (is_dir($source)) {
-
-                @mkdir($target);
-
-                $dir = dir($source);
-
-                while (($entry = $dir->read()) !== false) {
-                    if ($entry === '.' || $entry === '..') {
-                        continue;
-                    }
-
-                    fullCopy(
-                        $source . DIRECTORY_SEPARATOR . $entry,
-                        $target . DIRECTORY_SEPARATOR . $entry
-                    );
-                }
-
-                $dir->close();
-            } else {
-                copy($source, $target);
-            }
-        }
-    }
+    $dir = dirname(__DIR__) . $ds . 'src' . $ds . 'Packages' . $ds . 'Updater' . $ds . 'BaseStructure' . $ds;
 
     fullCopy(
-        'src' . $ds . 'Packages' . $ds . 'Updater' . $ds . 'BaseStructure' . $ds,
+        $dir,
         $_SERVER['DOCUMENT_ROOT'] . $ds
     );
 }

961490590f9174f8061e919bc15b1d8b7ba626e4
loppys <xteri.yt@gmail.com>
2023-01-24 22:11:11 +0300

    GUID

--- src/config/routesApi.php
+++ src/config/routesApi.php
@@ -1,5 +1,15 @@
 <?php
 
+use Vengine\Api\GUID\GuidController;
+use Vengine\System\Settings\Storages\MethodType;
+
 return [
-    
+    [
+        'route' => '/user/guid/generate/[{login}]',
+        'method' => MethodType::POST,
+        'handler' => [
+            'controller' => GuidController::class,
+            'method' => 'indexAction'
+        ]
+    ],
 ];

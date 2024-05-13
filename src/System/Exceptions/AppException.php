81baff5852eeeb1996ed2784160063917870a20f
loppys <xteri.yt@gmail.com>
2024-05-13 12:42:11 +0300

    up

--- src/System/Exceptions/AppException.php
+++ src/System/Exceptions/AppException.php
@@ -10,7 +10,7 @@ class AppException extends Exception
     public function __construct($message = "", $code = 500, Throwable $previous = null)
     {
         http_response_code($code);
-        if ($code !== 404) {
+        if (!empty($message)) {
             print json_encode(['message' => $message, 'code' => $code], JSON_THROW_ON_ERROR);
         }
 

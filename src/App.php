e3113f155ac164b935563c302ec6c50919a89526
loppys <xteri.yt@gmail.com>
2024-05-14 23:57:44 +0300

    cr

--- src/App.php
+++ src/App.php
@@ -276,7 +276,7 @@ final class App implements Injection
 
     public function logWriter(): void
     {
-        error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+        error_reporting(E_ALL & ~E_NOTICE);
         ini_set('display_errors', 'Off');
         ini_set('log_errors', 'On');
         ini_set('error_log', $_SERVER['DOCUMENT_ROOT'] . '/logs/errors.log');

10f799137d2a5e2ee32d5aedc5d74b26c7830577
loppys <xteri.yt@gmail.com>
2024-05-14 23:56:28 +0300

    cr

--- src/Packages/Updater/BaseStructure/www/index.php
+++ src/Packages/Updater/BaseStructure/www/index.php
@@ -4,11 +4,4 @@ use Vengine\App;
 
 require_once('../vendor/autoload.php');
 
-try {
-    (new App())->run();
-} catch (ReflectionException $e) {
-} catch (\Vengine\System\Exceptions\AccessDeniedException $e) {
-} catch (\Vengine\System\Exceptions\MethodNotAllowedException $e) {
-} catch (\Vengine\System\Exceptions\PageNotFoundException $e) {
-} catch (\Vengine\System\Exceptions\AppException $e) {
-}
+(new App())->run();
